d0a43eb9a27afc5393a9652263ebd952933e4345
ap058175 <ap058175@gmail.com>
2025-03-01 16:23:27 +0530

    diff() added

--- src/main/java/gfg_amit/gfg_amit/MyCalc.java
+++ src/main/java/gfg_amit/gfg_amit/MyCalc.java
@@ -5,10 +5,18 @@ public class MyCalc {
 	public int sum(int a, int b) {
 		return a+b;
 	}
+	
+	
+	public int diff(int a, int b) {
+		return a-b;
+	}
+	
+	
 
 	public static void main(String[] args) {
 	  MyCalc calc=new MyCalc();
 	  System.out.println("Sum is :"+ calc.sum(20, 10));
+	  System.out.println("Sum is :"+ calc.diff(20, 10));
 
 	}
 
